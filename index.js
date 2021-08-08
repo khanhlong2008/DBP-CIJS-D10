a9dad612321459a4c2885f3b1de5cc5ea7a653a7
khanhlong2008 <khanhlong11a1@gmail.com>
2021-08-09 00:37:27 +0700

    homeWork2

--- index.js
+++ index.js
@@ -1,72 +1,94 @@
-//Problem 1
-let Users = ([
-	{name: "Angelina Jolie",  age: 8},
-	{name: "Eric Jones",  age: 2},
-	{name: "Paris Hilton",  age: 5},
-	{name: "Kayne West", age: 16},
-	{ name: "Bob Ziroll", age: 100}
-]);
-Users = Users.map((usersItem)=>{
-  return(`<div><h1> ${usersItem.name} <h1><h2> ${usersItem.age} <h2><div>`)
+// hihi leetCode 
+//Problem 1:
+var reverse = function(x) {
+  const isNegative = x< 0 ? true : false;
+  
+  if (isNegative){
+      x = x *-1;
   }
-)
-// console.log(Users)
-
-//Problem 2 
-function students(People)
-{
- let result = []; 
-  for (let i = 0; i < People.length; i++){
-    let ele = People[i];
-    if(!(People[i].gender === 'female')){
-      result.push(ele)
-    }
+  
+  let reversed = 0;
+  while(x>0){
+      reversed = (reversed * 10) + (x % 10);
+      
+      x = parseInt(x/10);
   }
-  return result;
-}
-// console.log(students ([
-//   { name: 'Lan', gender: 'female' },
-//   { name: 'Linh', gender: 'female' },
-//   { name: 'Trung', gender: 'male' },
-//   { name: 'Peter', gender: 'gay' }
-// ]));
-function bai2(arr = []){
-  return arr.filter((item) => item.gender !== 'female')
-}
-// console.log(bai2([
-//    { name: 'Lan', gender: 'female' },
-//    { name: 'Linh', gender: 'female' },
-//    { name: 'Trung', gender: 'male' },
-//   { name: 'Peter', gender: 'gay' }
-//   ]))
-
-
-//Problem 3
-function Anagrams (strs){
-  const result = {};
-  for(let i = 0; i < strs.length; i++){
-    let sort = strs[i].split('').sort().join('');
-    if(!result[sort]){
-      result[sort] = [strs[i]]
-    }else{
-      result[sort].push(strs[i])
-    }
+  
+  if(reversed > 2**31){
+      return 0;
   }
-  return Object.values(result)
-}
-// console.log(Anagrams(["eat","tea","tan","ate","nat","bat"]))
-
-//Problem 4 
- function  singleNumber (nums) {
-  //  if(nums.length == 1){
-  //    return arr[0];
-  //  }
-  var tmp = 0;
-  for (var i = 0; i < nums.length; i++) {
-    tmp ^= nums[i];
-  }
-  return tmp;
+  
+  return isNegative? reversed * -1 : reversed;
 };
-// console.log(singleNumber([2,2,1]))
-// console.log(singleNumber([4,3,2,3,2]))
-console.log(singleNumber([1]))
+console.log(reverse(-123));
+//Problem 2:
+var romanToInt = function(s){
+    const map = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000, };
+
+    let result = 0;
+
+    for (i=0; i < s.length; i++){
+        const now = map[s[i]];
+        const next = map[s[i+1]];
+        if (now < next){
+            result += next - now 
+            // nếu now < next thì next - now vd:IV -> 5 - 1 = 4
+            i++
+        } else {
+            result += now
+            // ngược lại thì cộng dồn lại
+        }
+    } 
+    return result; 
+}
+
+// console.log(romanToInt("MCMXCVV"));
+
+
+//Problem 3:
+
+const API_url = 'https://60ae0d5e80a61f00173324bc.mockapi.io/products';
+var lastUrl = ""
+getProduct(API_url)
+
+function getProduct(url){
+  lastUrl = url;
+  fetch(url)
+  .then((res) => res.json())
+  .then((data) =>{
+      console.log(data)
+      renderProduct(data);      
+  })
+}
+
+
+
+function renderProduct(data){
+  const main = document.querySelector("#main");
+  main.innerHTML = "";
+ 
+  data.forEach((product) =>{
+  const {id,errorDescription,name,sku,image,color} = product;
+  const imgdefault  = "https://lh3.googleusercontent.com/Tv5UeL3K9mNR0iusP5wNRwZxJtgyxARUKddpKeMrgqb8FS0xJauJV2gddSDfiZPHxwJVWeHRHVFVB9qRcw=w500-rw"
+  main.insertAdjacentHTML("beforeend",
+   `
+   <div  class="card">
+      <img src="${image || imgdefault }" alt="" class="card-image"/>
+      <div class="card-content">
+        <div class="card-top">
+          <h3 class="card-title">Name: ${name}</h3>
+          <h3 class="card-title">Error: ${errorDescription}</h3>
+          <h3 class="card-title">SKU: ${sku}</h3>
+          <h3 class="card-title">ID: ${id}</h3>
+          <h3 class="card-title">Color: ${color || 0}</h3>
+        </div>     
+      </div>
+  </div>    
+  
+  `);
+   main.insertAdjacentHTML
+  });
+  
+
+  
+}
