5e94f7711a7784f2f06cdd6ac172c1ccf8c1eb49
khanhlong11a1@gmail.com <khanhlong11a1@gmail.com>
2021-08-10 01:33:15 +0700

    update link Car

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import React, { Component } from "react";
-// import Car from "./lecture4/Car/Car";
-import SmartphoneStore from "./lecture4/SmartphoneStore/SmartstoreComponent";
+import Car from "./lecture4/Car/Car";
+// import SmartphoneStore from "./lecture4/SmartphoneStore/SmartstoreComponent";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Car /> */}
-        <SmartphoneStore />
+        <Car />
+        {/* <SmartphoneStore /> */}
       </div>
     );
   }
