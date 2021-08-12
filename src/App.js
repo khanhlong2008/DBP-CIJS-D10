2219f6716e389c2915ffc9e10d7b71814fef963d
khanhlong11a1@gmail.com <khanhlong11a1@gmail.com>
2021-08-12 17:14:24 +0700

    update carts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import React, { Component } from "react";
-import LDMajor from "./lecture4/Burger/LDMajor";
+// import LDMajor from "./lecture4/Burger/LDMajor";
 // import Car from "./lecture4/Car/Car";
-// import SmartphoneStore from "./lecture4/SmartphoneStore/SmartstoreComponent";
+import SmartphoneStore from "./lecture4/SmartphoneStore/SmartstoreComponent";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         {/* <Car /> */}
-        {/* <SmartphoneStore /> */}
-        <LDMajor/>
+        <SmartphoneStore />
+        {/* <LDMajor/> */}
       </div>
     );
   }
--- src/lecture4/Burger/UpdateAmout.jsx
+++ src/lecture4/Burger/UpdateAmout.jsx
@@ -1,36 +1,36 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { Component } from "react";
-import "./../Burger/burger.css";
-class UpdateAmout extends Component {
-    constructor(props){
-        super(props)
+// import { Component } from "react";
+// import "./../Burger/burger.css";
+// class UpdateAmout extends Component {
+//     constructor(props){
+//         super(props)
         
-    }
-  render() {
-  const {updateMore,updateLess}= this.props
-  var {quatity}= this.props.selectedburgerBuilder
-    return (
-      <div className="row">
-        <input
-          type="button"
-          value="+"
-          className="btn1 btn-info col"
-          onClick={() =>
-            updateMore(quatity +1)
-          }
-        />
-        <p className="col">{quatity}</p>
-        <input
-          type="button"
-          value="-"
-          className="btn1 btn-info col"
-          onClick={() =>
-            updateLess(quatity -1)
-          }
-        />
-      </div>
-    );
-  }
-}
-export default UpdateAmout;
+//     }
+//   render() {
+//   const {updateMore,updateLess}= this.props
+//   var {quatity}= this.props.selectedburgerBuilder
+//     return (
+//       <div className="row">
+//         <input
+//           type="button"
+//           value="+"
+//           className="btn1 btn-info col"
+//           onClick={() =>
+//             updateMore(quatity +1)
+//           }
+//         />
+//         <p className="col">{quatity}</p>
+//         <input
+//           type="button"
+//           value="-"
+//           className="btn1 btn-info col"
+//           onClick={() =>
+//             updateLess(quatity -1)
+//           }
+//         />
+//       </div>
+//     );
+//   }
+// }
+// export default UpdateAmout;
