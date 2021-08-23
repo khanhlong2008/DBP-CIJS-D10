c33ffd11421c1f51ba547241215e119b89b008e1
khanhlong11a1@gmail.com <khanhlong11a1@gmail.com>
2021-08-23 20:54:41 +0700

    life  cycle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Counter from "./DemoLifeCycle/Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowCounter: true,
+    };
+    console.log("App: constructor");
+  }
+
+  onRemoveCounter = () => {
+    this.setState({
+      isShowCounter: false,
+    });
+  };
+
+  render() {
+    console.log("App: render");
+    return (
+      <div>
+        <button onClick={this.onRemoveCounter}>Remove Counter</button>
+        {/* {this.state.isShowCounter === true ? <Counter /> : null} */}
+        {this.state.isShowCounter && <Counter />}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    console.log("App: Mount");
+  }
+  componentDidUpdate() {
+    console.log("App: Update");
+  }
+  componentWillUnmount() {
+    console.log("App: Unmount");
+  }
 }
 
 export default App;
---
+++ src/DemoLifeCycle/Counter.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    console.log("Counter: constructor");
+  }
+
+  onDecrease = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  onIncrease = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  // Shallow Comparison => So sanh object for...of key 
+  /*
+    loahad
+    state: {
+        name: "hung",
+        age: 22,
+        info: {
+            address: "",
+            phoneNumber:"",
+            subInfo
+        }
+    }
+  */
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.count !== this.state.count) {
+      return true;
+    }
+
+    return false;
+  }
+
+  render() {
+    console.log("Counter: render");
+    return (
+      <div>
+        <button onClick={this.onDecrease}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.onIncrease}>+</button>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    console.log("Counter: Mount");
+  }
+
+  componentDidUpdate() {
+    console.log("Counter: Update");
+  }
+
+  componentWillUnmount() {
+    console.log("Counter: Unmount");
+  }
+}
+
+export default Counter;
