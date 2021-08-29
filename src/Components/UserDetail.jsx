f7278bba06db215e8a4d8a19ae608c76352f9d22
khanhlong11a1@gmail.com <khanhlong11a1@gmail.com>
2021-08-29 22:08:31 +0700

    complete

--- src/Components/UserDetail.jsx
+++ src/Components/UserDetail.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 import "./../Styles/UserDetail.css";
+import { useHistory } from "react-router-dom";
 
-class UserDetails extends Component {
-  render() {
-    const { repos, users } = this.props;
+const UserDetails = (props) =>{
+  const { repos, users } = props;
+
+    const history = useHistory();
+    const onBackToHome = ()=>{
+      history.goBack(`https://api.github.com/search/users?q=${users.login}`)
+    }
     return (
       <div className="container">
+        <input 
+        class="btn btn-secondary" 
+        type="button" 
+        value="Back to home"
+        onClick={onBackToHome}
+        />
         <div className="userInfo">
           <img
             src={users.avatar_url}
@@ -28,7 +39,7 @@ class UserDetails extends Component {
         </div>
       </div>
     );
-  }
+  
 }
 
 export default UserDetails;
