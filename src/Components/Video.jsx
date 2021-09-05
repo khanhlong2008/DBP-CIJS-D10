6bdbbd2ec986c50c50e6fb401b52eee05d1fcc32
khanhlong11a1@gmail.com <khanhlong11a1@gmail.com>
2021-09-05 22:57:03 +0700

    key undefine

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -4,13 +4,14 @@ class Video extends Component {
   render() {
     const { video} = this.props;
     const {key,name } = video
-    console.log(key)
     const URL_VIDEO = "https://www.youtube-nocookie.com/embed/";
+    // const DEAFULT_VIDEO = "https://www.youtube-nocookie.com/embed/N6DY_IkzPCI";
+    // || DEAFULT_VIDEO
     return <div className="video">
          <iframe
           width="1320"
           height="600"
-          src={URL_VIDEO + key}
+          src={URL_VIDEO + key }
           title={name}
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
